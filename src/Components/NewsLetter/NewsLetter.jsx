426a530dc1bdf828c23d13e6e6f4d02bb1c231dd
prasanna0911 <mechboyprasanna01@gmail.com>
2024-08-27 12:59:06 +0530

    UI correctiond

--- src/Components/NewsLetter/NewsLetter.jsx
+++ src/Components/NewsLetter/NewsLetter.jsx
@@ -4,12 +4,14 @@ import './NewsLetter.css'
 const NewsLetter = () => {
   return (
     <div className='newsletter'>
+      <div className='newslettertext'>
         <h1>Get Exclusive Offers On Your Email</h1>
         <p>Subscribe to our newletter and stay updated</p>
-        <div>
-            <input type='email' placeholder='Your Email id' />
-            <button>Subscribe</button>
-        </div>
+      </div>
+      <div className='newsletterbox'>
+        <input type='email' placeholder='Your Email id' />
+        <button>Subscribe</button>
+      </div>
     </div>
   )
 }
